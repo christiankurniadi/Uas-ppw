73c68d49815627e6992e4f541e65fed6993fde5f
Chris <christiankurniadi@gmail.com>
2022-12-20 00:02:25 +0700

    slight changes

--- admin/index.php
+++ admin/index.php
@@ -144,8 +144,6 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
-
                 <p>New Orders</p>
               </div>
               <div class="icon">
@@ -159,8 +157,6 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-
                 <p>Bounce Rate</p>
               </div>
               <div class="icon">
@@ -174,8 +170,6 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
-
                 <p>User Registrations</p>
               </div>
               <div class="icon">
@@ -189,8 +183,6 @@
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
-                <h3>65</h3>
-
                 <p>Unique Visitors</p>
               </div>
               <div class="icon">
